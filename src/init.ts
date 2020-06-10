1abe124a18c626f0b149046e642bddcb5dcfdb37
Christoph Neumann <c.neumann@schuetze-consulting.ag>
2020-06-10 16:48:03 +0200

    remove logging

--- src/init.ts
+++ src/init.ts
@@ -2,15 +2,12 @@ import { read, Document } from "./utils/lineparser"
 import JavascriptPlugin from "./utils/js/javascript-plugin"
 
 const plugins = [JavascriptPlugin]
-let count = 0
 export interface Plugin {
     accept(fileExt: string): boolean
     execute(document: Document): void
 }
 
 export function init(codemirror: Element): void {
-    console.log("init called " + count++)
-
     const document: Document = read(codemirror)
 
     const plugins = getPluginsForPath(document)
