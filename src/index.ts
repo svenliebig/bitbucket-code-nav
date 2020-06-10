b894918579f27dbb1305757ecee7ac406bdc9d91
Christoph Neumann <c.neumann@schuetze-consulting.ag>
2020-06-10 16:46:42 +0200

    add observer to follow dom changes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
 import { init } from "./init"
+
+const registerOberserver = (container: Element | null) => {
+    if (container === null) {
+        return
+    }
+    const reInit = () => {
+        const codemirror = document.querySelector(".content-view.fully-loaded")
+        if (codemirror) {
+            init(codemirror)
+        }
+    }
+
+    const mutationObserver = new MutationObserver(reInit)
+
+    mutationObserver.observe(container, {
+        attributes: true,
+    })
+}
+
 ;(function () {
     const timeout = setInterval(() => {
         const codemirror = document.querySelector(".content-view.fully-loaded")
 
         if (codemirror) {
+            registerOberserver(codemirror.querySelector(".CodeMirror-sizer>div"))
+
             clearTimeout(timeout)
 
             init(codemirror)
--- src/init.ts
+++ src/init.ts
@@ -2,13 +2,15 @@ import { read, Document } from "./utils/lineparser"
 import JavascriptPlugin from "./utils/js/javascript-plugin"
 
 const plugins = [JavascriptPlugin]
-
+let count = 0
 export interface Plugin {
     accept(fileExt: string): boolean
     execute(document: Document): void
 }
 
 export function init(codemirror: Element): void {
+    console.log("init called " + count++)
+
     const document: Document = read(codemirror)
 
     const plugins = getPluginsForPath(document)
