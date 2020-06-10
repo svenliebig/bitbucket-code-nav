95999c4cf3b3e189e40c6c4c9c44057b9d91ceaa
Christoph Neumann <c.neumann@schuetze-consulting.ag>
2020-06-10 17:47:23 +0200

    check if import exists

--- src/utils/js/javascript-plugin.ts
+++ src/utils/js/javascript-plugin.ts
@@ -1,6 +1,6 @@
 import { Plugin } from "../../init"
 import { Document, Line } from "../lineparser"
-import { resolve } from "url"
+import { resolve, parse, format } from "url"
 
 const importRegex = /import\s[\r\n\s\w{},*\$]*\sfrom\s['"](.+)['"]/
 
@@ -24,12 +24,17 @@ const importPath = (importLine: string): string => {
 type ImportLink = {
     element: Element
     url: string
+    restUrl: string
 }
 
 const toImportLink = ({ element, value }: Line, { path, fileExt }: Document): ImportLink => {
     const importFile = importPath(value) + fileExt
     const url = resolve(path, importFile)
-    return { element: element, url: url }
+
+    const restUrl = parse(url)
+    restUrl.pathname = "/rest/api/latest" + restUrl.pathname
+
+    return { element: element, url: url, restUrl: format(restUrl) }
 }
 
 const JavascriptPlugin: Plugin = {
@@ -42,7 +47,8 @@ const JavascriptPlugin: Plugin = {
             .filter((line) => importFilter(line.value))
             .map((line) => toImportLink(line, document))
             .forEach(async (importLink) => {
-                const response = await fetch(importLink.url)
+                const response = await fetch(importLink.restUrl)
+                debugger
                 if (response.ok) {
                     const link = window.document.createElement("a")
                     link.setAttribute("href", importLink.url)
